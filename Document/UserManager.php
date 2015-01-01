04a3932b2ef2e388a77d4594860d986bd463ec6e
Adam Bozsanyi <hunslater@gmail.com>
2015-01-01 21:11:53 +0100

    mongodb

--- Document/UserManager.php
+++ Document/UserManager.php
@@ -30,4 +30,54 @@ class UserManager extends BaseUserManager implements UserManagerInterface
     {
         return $this->repository->findBy($criteria, $orderBy, $limit, $offset);
     }
+
+    /**
+     * @param array $criteria
+     * @param integer $page
+     * @param integer $limit
+     * @param array $sort
+     *
+     * @return \Sonata\DatagridBundle\Pager\PagerInterface
+     */
+    public function getPager(array $criteria, $page, $limit = 10, array $sort = array())
+    {
+        $query = $this->repository
+            ->createQueryBuilder('u')
+            ->select('u');
+
+        $fields = $this->objectManager->getClassMetadata($this->class)->getFieldNames();
+        foreach ($sort as $field => $direction) {
+            if (!in_array($field, $fields)) {
+                unset($sort[$field]);
+            }
+        }
+        if (count($sort) == 0) {
+            $sort = array('username' => 'ASC');
+        }
+        foreach ($sort as $field => $direction) {
+            $query->orderBy(sprintf('u.%s', $field), strtoupper($direction));
+        }
+
+        $parameters = array();
+
+        if (isset($criteria['enabled'])) {
+            $query->andWhere('u.enabled = :enabled');
+            $parameters['enabled'] = $criteria['enabled'];
+        }
+
+        if (isset($criteria['locked'])) {
+            $query->andWhere('u.locked = :locked');
+            $parameters['locked'] = $criteria['locked'];
+        }
+
+        $query->setParameters($parameters);
+
+        $pager = new Pager();
+        $pager->setMaxPerPage($limit);
+        $pager->setQuery(new ProxyQuery($query));
+        $pager->setPage($page);
+        $pager->init();
+
+        return $pager;
+    }
 }
